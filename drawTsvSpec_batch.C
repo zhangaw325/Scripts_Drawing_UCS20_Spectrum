ae90fb0c57ae0955bda3c0c10aadb39667500b8b
zhangaw325 <zhangaw325@gmail.com>
2015-06-18 16:15:57 -0400

    Create drawTsvSpec_batch.C

---
+++ drawTsvSpec_batch.C
@@ -0,0 +1,69 @@
+// Introduction: we take spectra with SPECTECH UCS20 Multi Channel Analyzer with its offical software.
+//               the default saved spectrum files (*.spu) can be again saved as *.tsv files (tab seperated file).
+//               the *.tsv files can be read by this script and so that better spectra can be histogrammed with ROOT.
+//  -> This script can handle either one or more *.tsv files.
+// Author: Aiwu Zhang
+//
+#include <TString>
+void drawOneSpectrumTSV(string thefile){
+	string filename = thefile+".tsv";
+  string hTitle = thefile;
+  string rootfile = thefile+".root";
+  fstream fin(filename.c_str(),ios::in);
+  string lineContent;
+  TString theTimeString;
+
+  double time=0;
+  for(int i=0;i<22;i++){
+    getline(fin,lineContent);
+    if(lineContent.substr(0,43)=="Calibration Coefficients:Elapsed Real Time:"){
+    	theTimeString=lineContent.substr(44);
+    	time=theTimeString.Atof();
+    	//cout<<time<<"\t"<<time/100<<endl;
+    }
+  }
+  
+  TH1F* hist = new TH1F(hTitle.c_str(),"",2048,0,2048);
+  int bin=0;
+  float number=0;
+  int count=0;
+  while(fin>>bin>>number){
+    hist->SetBinContent(bin,number);
+    count+=number;
+  }
+  fin.close();
+  
+  hist->SetXTitle("ADC count");
+  hist->SetYTitle("Counts per bin per second");
+  hist->SetTitleSize(0.04,"XY");
+  hist->SetLabelSize(0.04,"XY");
+  hist->SetEntries(count);
+  TCanvas* c = new TCanvas();
+  hist->Draw();
+  //hist->SaveAs(rootfile.c_str());
+  hist->Write();
+  c->Close();
+}
+
+void main(){
+	TFile* f = new TFile("SpectraData.root","recreate");
+	string files[]={
+         "20150618_Ba133_PMT1.1kV_ampGain20",
+         "20150618_Ba133_PMT1.1kV_ampGain50",
+         "20150618_Ba133_PMT1.1kV_ampGain100",
+         "20150618_Co57_PMT1.1kV_ampGain50",
+         "20150618_Co57_PMT1.1kV_ampGain100",
+         "20150618_Co60_PMT1.1kV_ampGain20",
+         "20150618_Mn54_PMT1.1kV_ampGain20",
+         "20150618_Mn54_PMT1.1kV_ampGain50",
+         "20150618_Na22_PMT1.1kV_ampGain20",
+         "20150618_Na22_PMT1.1kV_ampGain50",
+         "20150618_unKnownSource_PMT1.1kV_ampGain50"
+		};
+			
+	
+	for(int i=0;i<60-50+1;i++){
+	  cout<<files[i]<<endl;
+	  drawOneSpectrumTSV(files[i]);
+	}
+}
